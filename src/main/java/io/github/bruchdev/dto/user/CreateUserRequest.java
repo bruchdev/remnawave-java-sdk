7a4d9f2809c34ca1e05dfdf3cd9bedfa095ad00a
Alex Bruch <alex.bruch@tutanota.com>
2025-11-06 05:53:04 +0100

    add compact constructor to CreateUserRequest

--- src/main/java/io/github/bruchdev/dto/user/CreateUserRequest.java
+++ src/main/java/io/github/bruchdev/dto/user/CreateUserRequest.java
@@ -2,7 +2,6 @@ package io.github.bruchdev.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.NonNull;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -11,24 +10,44 @@ import java.util.UUID;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public record CreateUserRequest(
-        @NonNull
         String username,
+        OffsetDateTime expireAt,
         Status status,
         String shortUuid,
         String trojanPassword,
         UUID vlessUuid,
         String ssPassword,
-        Long trafficLimitBytes,
+        Number trafficLimitBytes,
         TrafficLimitStrategy trafficLimitStrategy,
-        @NonNull
-        OffsetDateTime expireAt,
         OffsetDateTime createdAt,
         OffsetDateTime lastTrafficResetAt,
         String description,
         String tag,
-        String telegramId,
+        Number telegramId,
         String email,
         Integer hwidDeviceLimit,
-        List<UUID> activeInternalSquads
+        List<UUID> activeInternalSquads,
+        String uuid,
+        String externalSquadUuid
 ) {
+    public CreateUserRequest {
+        if (username == null) {
+            throw new IllegalArgumentException("Username is required");
+        }
+        if (expireAt == null) {
+            throw new IllegalArgumentException("ExpireAt is required");
+        }
+        if (trojanPassword.length() < 8 || trojanPassword.length() > 32) {
+            throw new IllegalArgumentException("trojanPasswordLength must be between 8 and 32 characters");
+        }
+        if (ssPassword.length() < 8 || ssPassword.length() > 32) {
+            throw new IllegalArgumentException("trojanPasswordLength must be between 8 and 32 characters");
+        }
+        if (tag.length() > 16) {
+            throw new IllegalArgumentException("Tag must be less than 16 characters");
+        }
+        if (telegramId.longValue() < 0) {
+            throw new IllegalArgumentException("TelegramId must be greater than 0");
+        }
+    }
 }
